a61e18c673b465df82566c6fb24204a676cdc436
vinntoe <lwent90@gmail.com>
2012-10-27 10:20:56 +0800

    1.8 1.9 eg1.5.2 eg 1.5.3

---
+++ 1.8.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+/*count space tab or lines in input*/
+int main(int argc, char const *argv[])
+{
+	int c,sl = tl = nl = 0;
+	while((c = getchar()) != EOF)
+		if (c == ' ')
+		{
+			sl++;
+		}
+		else if (c == '\t')
+		{
+			tl++;
+		}
+		else if (c == '\n')
+		{
+			nl++;
+		}
+	printf("%d\n", );
+	return 0;
+}
+
+/*
+officle answer 
+#include <stdio.h>
+
+int main(void)
+{
+  int blanks, tabs, newlines;
+  int c;
+  int done = 0;
+  int lastchar = 0;
+
+  blanks = 0;
+  tabs = 0;
+  newlines = 0;
+
+  while(done == 0)
+  {
+    c = getchar();
+
+    if(c == ' ')
+      ++blanks;
+
+    if(c == '\t')
+      ++tabs;
+
+    if(c == '\n')
+      ++newlines;
+
+    if(c == EOF)
+    {
+      if(lastchar != '\n')
+      {
+        ++newlines; // this is a bit of a semantic stretch, but it copes
+                    // with implementations where a text file might not
+                    // end with a newline. Thanks to Jim Stad for pointing
+                    // this out.
+                    ///
+      }
+      done = 1;
+    }
+    lastchar = c;
+  }
+  printf("Blanks: %d\nTabs: %d\nLines: %d\n", blanks, tabs, newlines);
+  return 0;
+}
+*/
---
+++ 1.9.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#ifndef IN
+#define IN 1
+#endif
+#ifndef OUT
+#define OUT 0
+#endif
+int main(int argc, char const *argv[])
+{
+	int c,state=OUT;
+	while((c = getchar()) != EOF){
+		if(c != ' '){
+			putchar(c);
+			state = IN;
+		}
+		else if (c == ' ' && state == IN){
+			putchar(c);
+			state = OUT;
+		}
+	}
+	putchar('\n');
+	return 0;
+}
+//the result is right :-)
